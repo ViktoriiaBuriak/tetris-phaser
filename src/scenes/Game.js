1aa4def0e12b6042671a728a6f25e78a71e3a230
ViktoriiaBuriak <vikiangelok@ukr.net>
2025-10-22 15:31:37 +0200

    add rotate

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,10 +6,62 @@ import { SHAPES } from "../utils/shapes";
 import { SHAPE_COLORS } from "../utils/shapes";
 import { board } from "../utils/constants";
 
+
+
 export default class Game extends Scene {
   constructor() {
     super("Game");
+    this.score = 0;
   }
+
+  create() {
+    this.bg = this.add.image(0, 0, "background").setOrigin(0);
+    this.displayWidth = this.bg.setDisplaySize(
+      BLOCK_SIZE * COLS,
+      BLOCK_SIZE * ROWS
+    );
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.spawnShape();
+
+    this.scoreText = this.add.text(516, 16, 'Score: 0', {
+      fontSize: '32px',
+      fill: '#fff'
+    })
+  }
+
+  update() {
+    if (!this.currentShape) return;
+
+    if (this.canMoveDown()) {
+      this.currentShape.forEach((block) => {
+        block.y += 1;
+      });
+    } else {
+      this.fixShape();
+      this.clearFullRows();
+      this.spawnShape();
+    }
+
+    if (Phaser.Input.Keyboard.JustDown(this.cursors.left)) {
+      this.moveShape(-1);
+    }
+
+    if (Phaser.Input.Keyboard.JustDown(this.cursors.right)) {
+      this.moveShape(1);
+    }
+
+    if (this.cursors.down.isDown) {
+      this.currentShape.forEach((block) => (block.y += 5));
+    }
+
+    if(Phaser.Input.Keyboard.JustDown(this.cursors.up)) {
+      this.rotateShape()
+    }
+  }
+
+  // Обираємо випадкову фігуру з const SHAPE
 
   spawnShape() {
     const bgCenterX = this.bg.x + this.bg.displayWidth / 2;
@@ -38,7 +90,10 @@ export default class Game extends Scene {
     });
 
     this.currentShape = blocks;
+
   }
+
+  // Фіксуємо фігуру
 
   fixShape() {
     for (let block of this.currentShape) {
@@ -49,42 +104,7 @@ export default class Game extends Scene {
     this.currentShape = null;
   }
 
-  create() {
-    this.bg = this.add.image(0, 0, "background").setOrigin(0);
-    this.displayWidth = this.bg.setDisplaySize(
-      BLOCK_SIZE * COLS,
-      BLOCK_SIZE * ROWS
-    );
-
-    this.cursors = this.input.keyboard.createCursorKeys();
-
-    this.spawnShape();
-  }
-
-  update() {
-    if (!this.currentShape) return;
-
-    if (this.canMoveDown()) {
-      this.currentShape.forEach((block) => {
-        block.y += 1;
-      });
-    } else {
-      this.fixShape();
-      this.spawnShape();
-    }
-
-    if (Phaser.Input.Keyboard.JustDown(this.cursors.left)) {
-      this.moveShape(-1);
-    }
-
-    if (Phaser.Input.Keyboard.JustDown(this.cursors.right)) {
-      this.moveShape(1);
-    }
-
-    if (this.cursors.down.isDown) {
-      this.currentShape.forEach((block) => (block.y += 5));
-    }
-  }
+  // Перевіряємо, чи можна зрушити фігуру вліво/вправо
 
   moveShape(dir) {
     let canMove = true;
@@ -111,6 +131,8 @@ export default class Game extends Scene {
     }
   }
 
+  // Перевіряємо, чи можна опустити фігуру на 1 рядок вниз.
+
   canMoveDown() {
     for (let block of this.currentShape) {
       const col = Math.floor(block.x / BLOCK_SIZE);
@@ -121,5 +143,75 @@ export default class Game extends Scene {
       if (board[row + 1][col] !== null) return false;
     }
     return true;
+  }
+
+  // Обертаємо фігуру
+
+  rotateShape() {
+    if(!this.currentShape) return;
+
+    const pivot = this.currentShape[0];
+
+    const newPosition = this.currentShape.map(block => {
+      const x = (block.x - pivot.x) / BLOCK_SIZE;
+      const y = (block.y - pivot.y) / BLOCK_SIZE;
+
+      const rotatedX = y;
+      const rotatedY = -x;
+
+      return {
+        x: pivot.x + rotatedX * BLOCK_SIZE,
+        y: pivot.y + rotatedY * BLOCK_SIZE,
+      }
+    })
+
+    for(let pos of newPosition) {
+      const col = Math.floor(pos.x / BLOCK_SIZE);
+      const row = Math.floor(pos.y / BLOCK_SIZE);
+
+      if (
+        col < 0 ||
+        col >= COLS ||
+        row < 0 ||
+        row >= ROWS ||
+        (board[row] && board[row][col] !== null)
+      ){
+        return;
+      }
+    }
+
+    this.currentShape.forEach((block, i) => {
+      block.x = newPosition[i].x;
+      block.y = newPosition[i].y;
+    })
+  }
+
+  // Очищуємо заповнені рядки
+
+  clearFullRows () {
+    for (let row = ROWS - 1; row >=0; row--) {
+      const isFull = board[row].every(cell => cell !== null);
+
+      if (isFull) {
+        for (let col = 0; col < COLS; col++) {
+          board[row][col].destroy();
+          board[row][col] = null;
+        }
+
+        for (let r = row - 1; r >= 0; r--) {
+          for (let c = 0; c < COLS; c++) {
+            const block = board[r][c];
+            if (block) {
+              block.y += BLOCK_SIZE;
+              board[r + 1][c] = block;
+              board[r][c] = null;
+            }
+          }
+        }
+        this.score += 10;
+        this.scoreText.setText('Score: ' + this.score);
+        row++;
+      }
+    }
   }
 }
